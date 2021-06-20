ac4e7bb4d7ece0b2c0f5a06fcf8e29b46123991e
pablo <26plau26@gmail.com>
2021-06-20 15:02:45 -0500

    get all image files from a folder

--- dora.py
+++ dora.py
@@ -1,7 +1,24 @@
 #!/usr/bin/env python3
 import PIL
 import os
-import sys
+import glob
+
+path = ""
+
+def get_files():
+    posfil = glob.glob(f"{path}/*.png")
+
+    for i in glob.glob(f"{path}/*.jpg"):
+        posfil.append(i)
+
+    for i in glob.glob(f"{path}/*.PNG"):
+        posfil.append(i)
+
+    for i in glob.glob(f"{path}/*.JPG"):
+        posfil.append(i)
+
+    for i in posfil:
+        print(i)
 
 if __name__ == "__main__":
-    pass
+    get_files()
