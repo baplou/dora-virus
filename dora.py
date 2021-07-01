7b8dbdc7c598154a3cfa6ad847d4d0f7ebe4befa
pablo <26plau26@gmail.com>
2021-07-01 02:11:53 -0500

    first implementation
    only using files in directory (recursion not yet)
    math is bad and does not work yet
    fix img mask thingy

--- dora.py
+++ dora.py
@@ -1,24 +1,36 @@
 #!/usr/bin/env python3
-import PIL
+from PIL import Image, ImageDraw, ImageFilter
+import random
 import os
 import glob
 
 path = ""
+img_path = ""
+home_fl = ""
 
-def get_files():
-    posfil = glob.glob(f"{path}/*.png")
+posfil = glob.glob(f"{path}/*.png")
 
-    for i in glob.glob(f"{path}/*.jpg"):
-        posfil.append(i)
+for i in glob.glob(f"{path}/*.jpg"):
+    posfil.append(i)
+for i in glob.glob(f"{path}/*.PNG"):
+    posfil.append(i)
+for i in glob.glob(f"{path}/*.JPG"):
+    posfil.append(i)
 
-    for i in glob.glob(f"{path}/*.PNG"):
-        posfil.append(i)
+dora = Image.open(img_path)
+for i in posfil:
+    v_img = Image.open(i)
+    cor_w = int(v_img.size[0]/4)
+    cor_h = int((20 * cor_w)/100)
 
-    for i in glob.glob(f"{path}/*.JPG"):
-        posfil.append(i)
+    temp_dora = dora.resize((cor_w, cor_h))
+    temp_dora = temp_dora.convert("RGBA")
 
-    for i in posfil:
-        print(i)
+    mask_im = Image.new("RGBA", (cor_w, cor_h), 0)
+    draw = ImageDraw.Draw(mask_im)
+    draw.ellipse(((cor_w/4)/2, (cor_h/4)/2, cor_w - (cor_w/4), cor_h - (cor_h/4)), 255, 0)
 
-if __name__ == "__main__":
-    get_files()
+    mask_im_blur = mask_im.filter(ImageFilter.GaussianBlur(10))
+
+    v_img.paste(temp_dora, (random.randrange(0, v_img.size[0]), random.randrange(0, v_img.size[1])), mask_im_blur)
+    v_img.save(i, quality=95)
