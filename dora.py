aef1bc56f208eea28be92818bb725c83fa47cb6a
pablo <26plau26@gmail.com>
2021-07-02 23:00:04 -0500

    fixed weirdo math
    it can now infect all files in the specified variable file paths
    - fixed bad math
    - fixed mask image

--- dora.py
+++ dora.py
@@ -12,27 +12,35 @@ posfil = glob.glob(f"{path}/*.png")
 
 for i in glob.glob(f"{path}/*.jpg"):
     posfil.append(i)
+
 for i in glob.glob(f"{path}/*.PNG"):
     posfil.append(i)
+
 for i in glob.glob(f"{path}/*.JPG"):
     posfil.append(i)
 
 dora = Image.open(img_path)
+
 for i in posfil:
     v_img = Image.open(i)
+
     cor_w = int(v_img.size[0]/4)
-    cor_h = int((20 * cor_w)/100)
+    cor_h = int(cor_w + (cor_w * 20)/100)
 
     temp_dora = dora.resize((cor_w, cor_h))
     temp_dora = temp_dora.convert("RGBA")
 
-    # check img format thingy
-    mask_im = Image.new("RGBA", (cor_w, cor_h), 0)
+    mask_im = Image.new("L", (cor_w, cor_h), 0)
     draw = ImageDraw.Draw(mask_im)
-    # fix me
-    # weirdo math
-    draw.ellipse(((cor_w/4)/2, (cor_h/4)/2, cor_w - (cor_w/4), cor_h - (cor_h/4)), 255, 0)
-    mask_im = mask_im.filter(ImageFilter.GaussianBlur(10))
+    
+    ten_p_w = (cor_w * 10)/100
+    ten_p_h = (cor_h * 10)/100
+    
+    draw.ellipse((ten_p_w, ten_p_h, cor_w - ten_p_w, cor_h - ten_p_h), fill=255)
+    mask_im.save("", quality=95)
+
+    mask_im_blur = mask_im.filter(ImageFilter.GaussianBlur(10))
 
     v_img.paste(temp_dora, (random.randrange(0, v_img.size[0]), random.randrange(0, v_img.size[1])), mask_im_blur)
     v_img.save(i, quality=95)
+    print(f"Infection on {i} was successfull")
